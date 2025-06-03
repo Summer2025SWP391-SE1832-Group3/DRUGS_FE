cfd3c536358780510f8951b5f517fe6dc54ecffe
anhvu <nguyentrananhvu04@gmail.com>
2025-06-03 14:32:14 +0700

    Update login.jsx, navbar.jsx, header.jsx, applayouts.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 // import './App.css'
-import Navbar from './components/layouts/Navbar'
-import Sidebar from './components/layouts/Sidebar'
-import Home from './pages/Home'
 import AppRoutes from './routes/appRoutes'
 function App() {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
-  const handleToggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-  }
+  
+  
 
   return (
     <>
-      {/* <Navbar onToggleSidebar={handleToggleSidebar} />
-      <Sidebar open={sidebarOpen} />
-      <main style={{ marginLeft: sidebarOpen ? 240 : 0, transition: 'margin-left 0.3s' }}>
-        
-      </main> */}
+      
       <AppRoutes />
     </>
   )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import { Form, Input, Button, Card, message } from 'antd'
+import { useNavigate, useOutletContext } from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const {setIsLoggedIn} = useOutletContext();
+  const fakeAccount = {
+    userName: 'anhvu',
+    password: '1'
+  }
+  const onFinish = (values) => {
+    if (values.username === fakeAccount.userName && values.password === fakeAccount.password) {
+      message.success('Đăng nhập thành công!');
+      setIsLoggedIn(true);
+      navigate('/courseList');
+    } else {
+      message.error('Đăng nhập thất bại! Vui lòng kiểm tra lại thông tin đăng nhập của bạn.');
+    }
+  }
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div style={{backgroundColor:"red"}}>Login</div>
+    <div style={{ minHeight: '80vh', background: '#f5f6fa', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <Card title="Login" style={{ minWidth: 350, maxWidth: 400, boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+        <Form layout="vertical" onFinish={onFinish} onFinishFailed={onFinishFailed}>
+          <Form.Item label="User Name" name="username" rules={[{ required: true, message: 'Please enter the username!' }]}>
+            <Input placeholder="Enter username" />
+          </Form.Item>
+          <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please enter the password!' }]}>
+            <Input.Password placeholder="Enter password" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              Đăng nhập
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   )
 }
